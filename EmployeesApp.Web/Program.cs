df07a6a69379599b6ba1c5321408495f42671b1c
bror <reachingbror@gmail.com>
2025-06-06 16:19:57 +0200

    wrong proj

--- EmployeesApp.Web/Program.cs
+++ EmployeesApp.Web/Program.cs
@@ -1,10 +1,12 @@
-﻿namespace EmployeesApp.Web
+﻿namespace EmployeesApp.Web;
+
+internal class Program
 {
-    internal class Program
-    {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello, World!");
-        }
-    }
+
+
+
+    
 }
+
+
+
