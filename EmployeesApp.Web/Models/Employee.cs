5d04340c751d7cf4a3a4b3f29e7ff987b7aa3f13
bror <reachingbror@gmail.com>
2025-06-08 00:07:30 +0200

    Adding Model

---
+++ EmployeesApp.Web/Models/Employee.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace EmployeesApp.Web.Models;
+
+public class Employee
+{
+
+    public int Id { get; set; }
+    public string Name { get; set; } = string.Empty;
+
+    [EmailAddress]
+    public string Email { get; set; } = string.Empty;
+
+
+
+
+
+
+
+
+
+
+
+}
--- EmployeesApp.Web/Program.cs
+++ EmployeesApp.Web/Program.cs
@@ -5,10 +5,9 @@ namespace EmployeesApp.Web
         public static void Main(string[] args)
         {
             var builder = WebApplication.CreateBuilder(args);
+            builder.Services.AddControllersWithViews();
             var app = builder.Build();
-
-            app.MapGet("/", () => "Hello World!");
-
+            app.MapControllers();
             app.Run();
         }
     }
