9619fda289bddf89969e98fb3b7569a9aa5c7140
bror <reachingbror@gmail.com>
2025-06-12 11:57:55 +0200

    Update .gitignore and enhance Employee validation
    - Added `MigrationBackup/` and `color.txt` to .gitignore.
    - Updated `Employee` class in `Employee.cs`:
      - Added required `Name` property with validation message.
      - Enhanced `Email` property with required attribute and updated error message.
    
    Working on part 2 of my assignment

--- EmployeesApp.Web/Models/Employee.cs
+++ EmployeesApp.Web/Models/Employee.cs
@@ -6,10 +6,14 @@ public class Employee
 {
 
     public int Id { get; set; }
+
+    [Required(ErrorMessage = "You must have a name! Enter name")]
     public string Name { get; set; } = string.Empty;
 
-    //[Required]
-    //[EmailAddress]
+   
+    
+    [Required(ErrorMessage ="Email is required")]
+    [EmailAddress(ErrorMessage ="Enter valid email please")]
     public string Email { get; set; } = string.Empty;
 
 
